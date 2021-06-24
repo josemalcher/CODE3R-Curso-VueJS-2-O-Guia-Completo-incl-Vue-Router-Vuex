989bda9d88d50fb65c5719faa9e610225065a8b4
José Malcher Jr <contato@josemalcher.net>
2021-06-24 17:15:31 -0300

    111. Hora de Praticar: Organizar Projeto em Componentes (Resposta)

---
+++ Secao-07-IntroducaoaosComponentes/111-Hora-de-Praticar-Organizar-Projeto-em-Componentes/componente-desafio/src/components/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-navigation-drawer app>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Menu
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <v-list dense class="pt-0">
+      <v-list-item v-for="item in itensMenu" :key="item.titulo">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.titulo }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      itensMenu: [
+        {titulo: 'Início', icon: 'mdi-view-dashboard'},
+        {titulo: 'Sobre', icon: 'mdi-forum'}
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Secao-07-IntroducaoaosComponentes/111-Hora-de-Praticar-Organizar-Projeto-em-Componentes/componente-desafio/src/components/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>Desafio</span>
+      <span class="font-weight-light">Componente</span>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Toolbar"
+}
+</script>
+
+<style scoped>
+
+</style>
