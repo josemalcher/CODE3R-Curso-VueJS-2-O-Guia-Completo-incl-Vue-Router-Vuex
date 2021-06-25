f0491a64461b60964bde34c61cf7e5ab1d89ca3a
José Malcher Jr <contato@josemalcher.net>
2021-06-24 23:02:27 -0300

    121. Usando Props no Componente Filho

--- Secao-08-Comunicacao-Entre-Componentes/117-Comunicacao-Entre-Componentes/comunicacao/src/components/UsuarioInfo.vue
+++ Secao-08-Comunicacao-Entre-Componentes/117-Comunicacao-Entre-Componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-      <p>nome do usuário:  <strong>{{ nome }}</strong></p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>nome do usuário: <strong>{{ inverterNome() }}</strong></p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['nome']
+  props: ['nome'],
+  methods: {
+    inverterNome(){
+      return this.nome.split('').reverse().join('')
+    }
+  }
 }
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
