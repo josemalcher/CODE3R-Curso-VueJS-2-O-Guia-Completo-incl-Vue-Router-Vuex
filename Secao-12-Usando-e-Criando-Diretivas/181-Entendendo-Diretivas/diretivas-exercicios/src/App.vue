d32479a944c7509c8e5436ba8ab0e1acdb356ca1
José Malcher Jr <contato@josemalcher.net>
2021-07-08 22:05:11 -0300

    Seção 12: Usando e Criando Diretivas - 189. Usando Múltiplos Modificadores

--- Secao-12-Usando-e-Criando-Diretivas/181-Entendendo-Diretivas/diretivas-exercicios/src/App.vue
+++ Secao-12-Usando-e-Criando-Diretivas/181-Entendendo-Diretivas/diretivas-exercicios/src/App.vue
@@ -7,32 +7,47 @@
     <p v-destaque:fundo.atrasar="'lightblue'">Testando Directivas</p>
     <p v-destaque.atrasar="cor">Testando...</p>
     <hr>
-    <p v-destaque-local:fundo.atrasar="'lightblue'">Testando Directivas</p>
+    <p v-destaque-local:fundo.atrasar.alternar="'lightblue'">Testando Directivas</p>
     <p v-destaque-local.atrasar="cor">Testando...</p>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   components: {},
   directives: {
     'destaque-local': {
       bind(el, binding, vnode) {
 
-        // eslint-disable-next-line no-unused-vars
+        const aplicarCor = cor => {
+          if (binding.arg === 'fundo') {
+            el.style.backgroundColor = cor;
+          } else {
+            el.style.color = cor;
+          }
+        }
+
         let atraso = 0;
+
+        const cor1 = binding.value;
+        const cor2 = 'purple';
+        let corAtual = cor1;
 
         if (binding.modifiers['atrasar']) {
           atraso = 3000;
         }
-        setTimeout(() => {
 
-          if (binding.arg === 'fundo') {
-            el.style.backgroundColor = binding.value;
-          } else {
-            el.style.color = binding.value;
-          }
-        },atraso);
+        setTimeout(() => {
+          setInterval(() => {
+            if (binding.modifiers['alternar']) {
+              corAtual = corAtual === cor1 ? cor2 : cor1;
+              aplicarCor(corAtual)
+            } else {
+              aplicarCor(binding.value)
+            }
+          }, 1000);
+        }, atraso);
       }
     }
   },
