2d1d2783730fd776a9bb0680dad87403c38eb624
José Malcher Jr <contato@josemalcher.net>
2021-07-15 10:54:31 -0300

    Seção 13: Melhorando sua App com Filtros e Mixins - 199. Duplicando Código para Usar os Mixins

--- Secao-13-Melhorando-sua-App-com-Filtros-e-Mixins/195-Introducao-do-Modulo/filtros-mixins-exercicios/src/App.vue
+++ Secao-13-Melhorando-sua-App-com-Filtros-e-Mixins/195-Introducao-do-Modulo/filtros-mixins-exercicios/src/App.vue
@@ -2,25 +2,43 @@
   <div id="app">
     <h1>Filtros & Mixins</h1>
     <hr>
-    <p>{{ cpfAluno | cpf | inverter}}</p>
+    <p>{{ cpfAluno | cpf | inverter }}</p>
     <input type="text" :value="cpfAluno | cpf | inverter">
+    <hr>
+    <Fruta/>
+    <hr>
+    <ul>
+      <li v-for="fruta in frutas" :key="fruta">{{fruta}}</li>
+    </ul>
+    <input type="text" v-model="fruta" @keydown.enter="add">
   </div>
 </template>
 
 <script>
+import Fruta from "./Fruta.vue";
+
 export default {
+  components: {Fruta},
   data() {
     return {
-      cpfAluno: '09856723412'
+      cpfAluno: '09856723412',
+      fruta: '',
+      frutas: ['banana', 'maçã', 'laranja']
     }
   },
   filters: {
     cpf(valor) {
       const arr = valor.split('');
-      arr.splice(3,0, '.')
-      arr.splice(7,0, '.')
-      arr.splice(11,0, '-')
+      arr.splice(3, 0, '.')
+      arr.splice(7, 0, '.')
+      arr.splice(11, 0, '-')
       return arr.join('')
+    }
+  },
+  methods: {
+    add() {
+      this.frutas.push(this.fruta)
+      this.fruta = ''
     }
   }
 }
@@ -36,7 +54,8 @@ export default {
   margin-top: 60px;
   font-size: 2.5rem;
 }
-input{
+
+input {
   font-size: 2.5rem;
 }
 </style>
