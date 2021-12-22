5ec2792e533be64a895c6d2cfbed1c5a692e62c4
José Malcher Jr <contato@josemalcher.net>
2021-12-22 20:02:40 -0300

    285. Mapeando Getters para Propriedades

--- Secao-17-Melhor-Gerenciamento-de-Estado-com-Vuex/vuex-exercicios-rev-01/src/components/Carrinho.vue
+++ Secao-17-Melhor-Gerenciamento-de-Estado-com-Vuex/vuex-exercicios-rev-01/src/components/Carrinho.vue
@@ -1,69 +1,70 @@
 <template>
-    <Painel titulo="Carrinho" azul :notificacao="produtos.length">
-        <div class="carrinho">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Qtde</th>
-                        <th>Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="produto in produtos" :key="produto.nome">
-                        <td>{{ produto.nome }}</td>
-                        <td>{{ produto.quantidade }}</td>
-                        <td>{{ produto.preco | dinheiro }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr>
-            <div class="total">
-                <span>Total: <strong>{{ total | dinheiro }}</strong></span>
-            </div>
-        </div>
-    </Painel>
+  <Painel titulo="Carrinho" azul :notificacao="produtos.length">
+    <div class="carrinho">
+      <table>
+        <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Qtde</th>
+          <th>Preço</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="produto in produtos" :key="produto.nome">
+          <td>{{ produto.nome }}</td>
+          <td>{{ produto.quantidade }}</td>
+          <td>{{ produto.preco | dinheiro }}</td>
+        </tr>
+        </tbody>
+      </table>
+      <hr>
+      <div class="total">
+        <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+      </div>
+    </div>
+  </Painel>
 </template>
 
 <script>
 export default {
-    computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        },
-        produtos() {
-          return this.$store.state.produtos
-        }
+  computed: {
+    total() {
+      return this.$store.getters.valorTotal;
+      // return this.produtos.map(p => p.quantidade * p.preco)
+      //     .reduce((total, atual) => total + atual, 0)
     },
-    // data() {
-    //     return {
-    //         produtos: [
-    //             { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-    //             { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-    //             { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-    //         ]
-    //     }
-    // }
+    produtos() {
+      return this.$store.state.produtos
+    }
+  },
+  // data() {
+  //     return {
+  //         produtos: [
+  //             { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
+  //             { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
+  //             { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
+  //         ]
+  //     }
+  // }
 }
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+table {
+  width: 100%;
+}
 
-    td {
-        border-top: 1px solid #EEE;
-        width: 33%;
-    }
+td {
+  border-top: 1px solid #EEE;
+  width: 33%;
+}
 
-    hr {
-        margin-top: 30px;
-    }
+hr {
+  margin-top: 30px;
+}
 
-    .total {
-        display: flex;
-        justify-content: flex-end;
-    }
+.total {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
--- Secao-17-Melhor-Gerenciamento-de-Estado-com-Vuex/vuex-exercicios-rev-01/src/components/Resumo.vue
+++ Secao-17-Melhor-Gerenciamento-de-Estado-com-Vuex/vuex-exercicios-rev-01/src/components/Resumo.vue
@@ -1,43 +1,53 @@
 <template>
-    <Painel titulo="Resumo" roxo>
-        <div class="resumo">
-            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
-            <hr>
-            <button>Finalizar!</button>
-        </div>
-    </Painel>
+  <Painel titulo="Resumo" roxo>
+    <div class="resumo">
+      <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+      <hr>
+      <button>Finalizar!</button>
+    </div>
+  </Painel>
 </template>
 
 <script>
+
+import {mapGetters} from 'vuex'
+
 export default {
-    computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        },
-      produtos() {
-          return this.$store.state.produtos
-      }
-    },
-    // data() {
-    //     return {
-    //         produtos: [
-    //             { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-    //             { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-    //             { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-    //         ]
-    //     }
+
+//  computed: mapGetters(['valorTotal'])
+computed:mapGetters({
+  total: 'valorTotal'
+
+})
+  // computed: {
+  //   total() {
+  //     return this.$store.getters.valorTotal;
+  //     // return this.produtos.map(p => p.quantidade * p.preco)
+  //     //     .reduce((total, atual) => total + atual, 0)
+  //   },
+    // produtos() {
+    //   return this.$store.state.produtos
     // }
+  // },
+  // data() {
+  //     return {
+  //         produtos: [
+  //             { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
+  //             { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
+  //             { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
+  //         ]
+  //     }
+  // }
 }
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+table {
+  width: 100%;
+}
 
-    td {
-        border-top: 1px solid #EEE;
-        width: 33%;
-    }
+td {
+  border-top: 1px solid #EEE;
+  width: 33%;
+}
 </style>
